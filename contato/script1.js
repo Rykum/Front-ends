3a7e0dcec8e27203de57dd0abdb4d524bbf7f241
Pedro Munhoz <Pedrolucaslmunhoz@gmail.com>
2024-06-24 12:30:28 -0300

    capa

--- contato/script1.js
+++ contato/script1.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const minhaimagem = document.querySelector('#minhaimagem');
     const colors = ['red', 'blue', 'green'];
 
-    // Função para iniciar a animação
-    function startAnimation() {
+    // Função para iniciar a animação de clique
+    function startClickAnimation() {
         // Mostra a sombra preta antes da animação principal
         const shadowOverlay = document.createElement('div');
         shadowOverlay.classList.add('shadow-overlay');
@@ -43,12 +43,29 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 5000); // Duração total da animação é de 5 segundos
     }
 
-    // Função auxiliar para gerar número aleatório entre um intervalo
-    function randomIntFromInterval(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
+    // Função para iniciar a animação de carregamento da página
+    function startPageLoadAnimation() {
+        const pageLoadOverlay = document.createElement('div');
+        pageLoadOverlay.classList.add('page-load-overlay');
+        document.body.appendChild(pageLoadOverlay);
+
+        // Iniciar a animação imediatamente
+        setTimeout(() => {
+            pageLoadOverlay.classList.add('active');
+        }, 0);
+
+        
     }
 
     // Adiciona evento de clique na imagem do logo para iniciar a animação
     const logoImage = document.querySelector('.logo img');
-    logoImage.addEventListener('click', startAnimation);
+    logoImage.addEventListener('click', startClickAnimation);
+
+    // Inicia a animação de carregamento da página
+    startPageLoadAnimation();
+
+    // Função auxiliar para gerar número aleatório entre um intervalo
+    function randomIntFromInterval(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
 });
